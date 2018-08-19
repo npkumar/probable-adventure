3462ae9e46ea4cb3023e9933a7d5250b708731f7
Nitin Pradeep Kumar <getnpk@gmail.com>
2018-08-19 19:07:37 +0900

    Refactor Circle as a class, add circles

--- bouncingCircles/canvas.js
+++ bouncingCircles/canvas.js
@@ -1,22 +1,67 @@
 var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 var context = canvas.getContext('2d');
-var r = Math.random() * 255;
-var b = Math.random() * 255;
-var g = Math.random() * 255;
 
-var x = Math.random() * window.innerWidth;
-var y = Math.random() * window.innerHeight;
-var radius = 30;
-var velocityFactor = 10;
 
-// velocity
-// decide on direction first, with random velocity
-var dx = (Math.random()  - 0.5) * velocityFactor;
-var dy = (Math.random() - 0.5) * velocityFactor;
+function Circle(x, y, dx, dy, radius, r, g, b) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.r = r;
+  this.g = b;
+  this.b = b;
 
+  this.draw = function() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.strokeStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+    // context.strokeStyle = 'red';
+    context.stroke();
+  }
+
+  this.update = function() {
+    if (this.x + this.radius > window.innerWidth || this.x - this.radius < 0 ) {
+      this.dx = -this.dx;
+    }
+
+    if (this.y + this.radius > window.innerHeight || this.y - this.radius < 0 ) {
+      this.dy = -this.dy;
+    }
+
+    // move x
+    this.x += this.dx;
+
+    // move y
+    this.y += this.dy;
+
+    this.draw();
+  }
+}
+
+var circles = [];
+var NUM_CIRCLES = 100;
+
+for (var i = 0; i < NUM_CIRCLES; i++) {
+  var r = Math.random() * 255;
+  var b = Math.random() * 255;
+  var g = Math.random() * 255;
+
+  var x = Math.random() * window.innerWidth;
+  var y = Math.random() * window.innerHeight;
+  var radius = Math.random() * 30;
+  var velocityFactor = 5;
+
+  // velocity
+  // decide on direction first, with random velocity
+  var dx = (Math.random()  - 0.5) * velocityFactor;
+  var dy = (Math.random() - 0.5) * velocityFactor;
+  circles.push(
+    new Circle(x, y, dx, dy, radius, r, g, b)
+  );
+}
 
 function animate() {
   requestAnimationFrame(animate);
@@ -24,24 +69,9 @@ function animate() {
   // clear first
   context.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
-  context.beginPath();
-  context.arc(x, y, radius, 0, Math.PI * 2, false);
-  context.strokeStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
-  context.stroke();
-
-  if (x + radius > window.innerWidth || x - radius < 0 ) {
-    dx = -dx;
-  }
-
-  if (y + radius > window.innerHeight || y - radius < 0 ) {
-    dy = -dy;
-  }
-
-  // move x
-  x += dx;
-
-  // move y
-  y += dy;
+  circles.forEach(function(circle) {
+    circle.update();
+  });
 }
 
 animate();
